d8430a298c99c35da0d5f9dc371d1dbd67684af3
plutoot <128110727+plutoot@users.noreply.github.com>
2023-03-18 12:02:45 -0400

    Add files via upload

---
+++ dictionary.js
@@ -0,0 +1,49 @@
+const medicalTerms = [
+    // A
+    {term: 'Allergies', definition: 'An abnormal reaction of the immune system to a harmless substance.'},
+    {term: 'Angina', definition: 'Chest pain or discomfort that you feel when there is not enough blood flowing to your heart.'},
+    {term: 'Asthma', definition: 'A long-term (or chronic) disease that leads the airways to your lungs to swell, making it hard to breathe.'},
+    {term: 'Anaesthetic', definition: 'A drug that reduces pain or that puts you to sleep as during surgery.'},
+    
+    // B
+    {term: 'Blood Thinners', definition: 'M[edicines that help blood flow smoothly without blood clots.'},
+    
+    // D
+    {term: 'Deep Vein Thrombosis', definition: 'A blood clot that forms in a vein deep in your body.'},
+    {term: 'DVT', definition: 'A blood clot that forms in a vein deep in your body.'},
+    {term: 'Dialysis', definition: 'A treatment that does the work of your kidneys when they can\'t.'},
+
+    // L
+    {term: 'Latex Allergy', definition: 'An abnormal reaction of the immune system to natural, rubber latex.'},
+
+    // H
+    {term: 'Hypertension', definition: 'High blood pressure'},
+
+    // I
+    {term: 'immunization', definition: 'giving a person a vaccine to protect them against a disease.'},
+
+    // M
+    {term: 'Malignant Hyperthermia', definition: 'A severe reaction to certain drugs used to reduce pain or put you to sleep (ie. anesthesia).'},
+    {term: 'Medication', definition: 'Medicine; substances used to treat, manage, or prevent medical conditions.'},
+    {term: 'medical condition', definition: 'any kind of illness that can affect your mood, thinking, or how you act.'},
+    
+    // P
+    {term: 'Physician', definition: 'A doctor.'},
+    {term: 'prescribed', definition: 'An order from your doctor, nurse, or physician assistant for drugs or other treatment.'},
+    {term: 'Pulmonary Embolism', definition: 'When a blood clot blocks in an artery in your lung suddenly.'},
+    
+    // R
+    {term: 'Referring Doctor', definition: 'Another doctor who redirected you to get a second opinion.'},
+    
+    // S
+    {term: 'Sleep Apnea', definition: 'When you stop breathing for 10 seconds or more when you sleep.'},
+    
+    // T
+    {term: 'Thyroid', definition: 'A gland in your neck that is shaped like a butterfly. It makes hormones that help your body work normally.'},
+    
+    // V
+    {term: 'Version Code', definition: '2-letter code at the end of the health number on an Ontario Health Card.'},
+    
+    // W
+    {term: 'WSIB', definition: 'Workplace Safety and Insurance Board'},
+]
