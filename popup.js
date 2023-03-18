85fbb637a22a35b11c6918654272f44bf4898c72
Yaxin <113393972+yaxinw04@users.noreply.github.com>
2023-03-18 13:16:52 -0400

    health care number autofill option update

--- popup.js
+++ popup.js
@@ -1,27 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-    chrome.storage.sync.get(["firstName", "lastName"], (result) => {
-      document.getElementById("firstName").value = result.firstName || ""; // sets value of html element firstName to result.firstName
-      document.getElementById("lastName").value = result.lastName || "";
-    });
-
+    chrome.storage.sync.get(
+      ["firstName", "lastName", "healthCardNumber"],
+      (result) => {
+        document.getElementById("firstName").value = result.firstName || ""; // sets value of html element firstName to result.firstName
+        document.getElementById("lastName").value = result.lastName || "";
+        document.getElementById("healthCardNumber").value = result.healthCardNumber || "";
+      }
+    );
+  
     const form = document.getElementById("autofillForm");
     form.addEventListener("submit", (event) => {
       event.preventDefault(); // wtf does this mean
   
       const firstName = document.getElementById("firstName").value;
       const lastName = document.getElementById("lastName").value;
+      const healthCardNumber = document.getElementById("healthCardNumber").value;
   
-      chrome.storage.sync.set({ firstName, lastName }, () => {
-        window.close();
+      chrome.storage.sync.set({ firstName, lastName, healthCardNumber }, () => {
+          // window.close();
       });
     });
-
+  
     document.getElementById("autofillButton").addEventListener("click", () => {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          chrome.tabs.sendMessage(tabs[0].id, { action: "autofill" });
-        });
-    
-        window.close();
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "autofill" });
       });
+  
+      window.close();
     });
-    
+  });
+  
